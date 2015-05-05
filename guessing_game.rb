c8f3ac9432e2982913ec7ae6c7a82a8e471dd2c9
Scott Williams <williamsjs1224@gmail.com>
2015-05-05 14:22:23 -0400

    Added another function

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,14 +1,16 @@
-def hot_or_cold(guess)
+def guessing(guess)
   random_num = rand(1..100)
   count = 0
+  hot_or_cold(guess, random_num, count)
+end
+
+def hot_or_cold(guess, random_num, count)
   while count < 4
     if guess > random_num
       puts "You are too high!"
-      puts "Sorry, the number was #{random_num}"
       guess = gets.chomp.to_i
     elsif guess < random_num
       puts "You are too low!"
-      puts "Sorry, the number was #{random_num}"
       guess = gets.chomp.to_i
     else
       puts "You win!"
@@ -16,9 +18,13 @@ def hot_or_cold(guess)
     end
     count += 1
   end
+  if count == 4
+    puts "Sorry, the number was #{random_num}"
+    puts "You lose!"
+  end
 end
 
 puts "Please guess a number between 1 and 100"
 guess = gets.chomp.to_i
 
-hot_or_cold(guess)
+guessing(guess)
