d02aed98c9a670424cb645307b52ae422d64c7d9
Scott Williams <williamsjs1224@gmail.com>
2015-05-05 18:32:10 -0400

    final touches on normal

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,5 +1,3 @@
-#NORMAL MODE----------------------------------
-
 def guessing(guess)
   random_num = rand(1..100)
   count = 0
@@ -15,51 +13,21 @@ def hot_or_cold(guess, random_num, count)
       puts "You are too low!"
     elsif guess > random_num
       puts "You are too high"
-    else
-      puts "You win!"
+    end
+
+    if guess == random_num
       break
     end
-    count += 1
 
     guessed_nums.push(guess)
+    count += 1
     guess = gets.chomp.to_i
   end
   guessed_nums.push(guess)
   puts "You guessed #{guessed_nums}"
-  if count == 4
-    puts "Sorry, the number was #{random_num}"
-    puts "You lose!"
-  end
-end
-
-puts "Please guess a number between 1 and 100"
-guess = gets.chomp.to_i
-
-guessing(guess)
-
-#------------------------------------------------
-
-def guessing(guess)
-  random_num = rand(1..100)
-  count = 0
-  hot_or_cold(guess, random_num, count)
-end
-
-def hot_or_cold(guess, random_num, count)
-  while count < 4
-    if guess > random_num
-      puts "You are too high!"
-      guess = gets.chomp.to_i
-    elsif guess < random_num
-      puts "You are too low!"
-      guess = gets.chomp.to_i
-    else
-      puts "You win!"
-      break
-    end
-    count += 1
-  end
-  if count == 4
+  if guessed_nums.include?(random_num)
+    puts "You won!"
+  else
     puts "Sorry, the number was #{random_num}"
     puts "You lose!"
   end
