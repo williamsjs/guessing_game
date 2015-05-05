a71a240dffc9363b1737f23b6770d2f0c8731649
Scott Williams <williamsjs1224@gmail.com>
2015-05-05 13:46:41 -0400

    added method hot or cold

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,3 +1,24 @@
-random_num = rand(1..100)
+def hot_or_cold(guess)
+  random_num = rand(1..100)
+  count = 0
+  while count < 4
+    if guess > random_num
+      puts "You are too high!"
+      puts "Sorry, the number was #{random_num}"
+      guess = gets.chomp.to_i
+    elsif guess < random_num
+      puts "You are too low!"
+      puts "Sorry, the number was #{random_num}"
+      guess = gets.chomp.to_i
+    else
+      puts "You win!"
+      break
+    end
+    count += 1
+  end
+end
 
-puts random_num
+puts "Please guess a number between 1 and 100"
+guess = gets.chomp.to_i
+
+hot_or_cold(guess)
